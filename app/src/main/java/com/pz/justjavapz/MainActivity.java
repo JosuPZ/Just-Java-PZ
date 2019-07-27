f86cb3c7bac12272d53e4685c4d692a3fbd93c54
Josu Pérez Zarraonandia <josu.perez.zarra@gmail.com>
2019-07-27 22:45:46 +0200

    Added functionality to the button
    When clicked, the button changes the number displayed on the
    quantity_text_view (it changes it from 0 to 1).

--- app/src/main/java/com/pz/justjavapz/MainActivity.java
+++ app/src/main/java/com/pz/justjavapz/MainActivity.java
@@ -1,14 +1,33 @@
 package com.pz.justjavapz;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+/**
+ * This app displays and order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view){
+        display(1);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number){
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
