8e57a4b45d78cf197d266c3ea12b9abf79b64847
Josu Pérez Zarraonandia <josu.perez.zarra@gmail.com>
2019-07-30 23:16:43 +0200

    Minor changes to code
    To be more rigorous with Java Syntax

--- app/src/main/java/com/pz/justjavapz/MainActivity.java
+++ app/src/main/java/com/pz/justjavapz/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void display(int number){
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText(Integer.toString(number));
     }
 
     /**
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(number + "€");
+        priceTextView.setText(Integer.toString(number) + "€");
     }
 
 }
