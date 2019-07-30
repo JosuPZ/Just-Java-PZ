ee37e41b08495c822550f50ae169a05c97937cda
Josu Pérez Zarraonandia <josu.perez.zarra@gmail.com>
2019-07-30 23:13:20 +0200

    Added displayPrice method

--- app/src/main/java/com/pz/justjavapz/MainActivity.java
+++ app/src/main/java/com/pz/justjavapz/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import org.w3c.dom.Text;
+import java.text.NumberFormat;
+
 
 /**
  * This app displays and order form to order coffee.
@@ -30,4 +33,13 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(number + "€");
+    }
+
 }
