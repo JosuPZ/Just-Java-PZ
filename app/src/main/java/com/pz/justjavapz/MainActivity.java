f9042d62e90270d6f0577176fff5ee12596718f9
Josu Pérez Zarraonandia <josu.perez.zarra@gmail.com>
2019-07-31 11:38:27 +0200

    Fixed minor bug
    Incorrect price was showing up when quantity=1

--- app/src/main/java/com/pz/justjavapz/MainActivity.java
+++ app/src/main/java/com/pz/justjavapz/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     public void decrement(View view) {
         int quantity = 1;
         display(quantity);
-        displayPrice(quantity);
+        displayPrice(quantity * 5);
     }
 
     /**
