1e4bb077cd70e5565058e4fdd02559fde1407328
Josu Pérez Zarraonandia <josu.perez.zarra@gmail.com>
2019-07-31 10:45:59 +0200

    Added variable to store number of ordered coffees

--- app/src/main/java/com/pz/justjavapz/MainActivity.java
+++ app/src/main/java/com/pz/justjavapz/MainActivity.java
@@ -23,8 +23,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view){
-        display(2);
-        displayPrice(2* 5);
+        int numberOfCoffees = 2;
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees* 5);
     }
 
     /**
